1b8035b4fcf2f2d0d0727d7a41684d12bd3f40be
Anuj Jhawar <anujjhawar28@gmail.com>
2026-01-21 11:44:27 +0530

    Added Test Component local to existing Component

--- 02-react-hello-world/src/App.tsx
+++ 02-react-hello-world/src/App.tsx
@@ -5,6 +5,18 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+
+  function TestComponent() {
+    const [heading, setHeading] = useState("Test Component Heading");
+    return (
+      <>  
+        <div>
+          {heading}
+        </div>
+        <button onClick={() => setHeading("New Heading")}>Change heading</button>
+      </>
+    )
+  }
   return (
     <>
       <h1>Vite + React</h1>
@@ -20,6 +32,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <HelloWorld />
+      <TestComponent />
     </>
   )
 }
